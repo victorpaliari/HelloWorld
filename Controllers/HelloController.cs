d7802ca0b0e053867a70438820459ee32c8ee4bb
Victor Paliari <victorrpaliari@gmail.com>
2023-09-21 17:02:21 -0300

    Hello World

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace helloworld.Controllers
+{
+    [Route("~/")]
+    //como se fosse a homepage da aplicação
+    [ApiController]
+    public class HelloController
+    {
+        [HttpGet]
+        public string Hello()
+        {
+            return "Hello Turma . NET 03!";
+        }
+
+        [HttpGet("/meunome")]
+        public string MeuNome()
+        {
+            return "Victor Paliari";
+        }
+    }
+}
